5b584c68793f33f86a89b1946be3b8fefd5b437a
ctoverton <christianoverton@ctoverton.com>
2023-07-02 22:38:26 -0600

    added icons config added images config

---
+++ src/config/icons.ts
@@ -0,0 +1,5 @@
+export const ICONS = {
+  GITHUB: '/icons/github-mark.svg',
+  HOME: '/icons/home.svg',
+  CALENDAR: '/icons/calendar-days-solid.svg',
+}
---
+++ src/config/images.ts
@@ -0,0 +1,11 @@
+export const IMAGES = {
+  TRANSACTIONS_COMPONENT:
+    '/images/componentsShowcase/transactions-component.svg',
+  CATEGORIES_COMPONENT: '/images/componentsShowcase/categories-component.svg',
+  MERCHANTS_COMPONENT: '/images/componentsShowcase/merchants-component.svg',
+
+  PERFORMANCE_METRICS: '/images/performance-metrics.svg',
+
+  JOURNEY_APP: '/images/appIcons/journey-app.svg',
+  DEV_DAWGS_APP: '/images/appIcons/dev-dawgs-app.svg',
+}
