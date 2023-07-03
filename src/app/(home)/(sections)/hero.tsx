d79569f5097e6229fcb1d6bb233a413aa7ef2828
ctoverton <christianoverton@ctoverton.com>
2023-07-02 22:24:53 -0600

    improved hero responsiveness

--- src/app/(home)/(sections)/hero.tsx
+++ src/app/(home)/(sections)/hero.tsx
@@ -7,9 +7,10 @@ export const HeroSection = () => {
       className={
         'relative min-h-screen overflow-x-hidden bg-floatplane py-8 text-white'
       }
+      id={'home'}
     >
-      <div className={'mx-auto mb-16 max-w-3xl px-4 text-center'}>
-        <div className={'text-display-3 mb-24'}>Christian Overton</div>
+      <div className={'mx-auto mb-16 max-w-4xl px-4 text-center'}>
+        <div className={'text-display-3 mb-16 sm:mb-24'}>Christian Overton</div>
 
         <div className={'text-subtitle mb-4'}>
           Front-End interactive cover letter
@@ -22,14 +23,14 @@ export const HeroSection = () => {
           href={'https://github.com/CTOverton/LMG-Cover-Letter'}
           target={'_blank'}
         >
-          <Button className={'mx-auto mb-16'} leftIcon={'/github-mark.svg'}>
+          <Button className={'mx-auto'} leftIcon={'/github-mark.svg'}>
             View source code
           </Button>
         </a>
 
         <div
           className={
-            'mx-auto max-w-xl rounded-xl border border-white px-3 py-2'
+            'mx-auto mt-8 max-w-xl rounded-xl border border-white px-3 py-2 sm:mt-16'
           }
         >
           This website was purpose built for this position with relevant
@@ -37,7 +38,7 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <ParallaxText />
+      <ParallaxText className={'absolute bottom-4 sm:bottom-24'} />
     </section>
   )
 }
