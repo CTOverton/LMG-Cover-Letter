0d9b08a367058cb862b37c136cd074a5c8ee51b8
ctoverton <christianoverton@ctoverton.com>
2023-07-02 22:39:34 -0600

    added variants to button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,22 +1,31 @@
+const variants = {
+  primary: '',
+  secondary: 'border-4 border-floatplane',
+}
+
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   leftIcon?: string
+  variant?: 'primary' | 'secondary'
 }
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   className,
   leftIcon,
+  variant = 'primary',
   ...other
 }) => {
   return (
     <button
-      className={`text-btn flex items-center rounded-full bg-white px-6 py-3 text-black transition-all hover:scale-110 ${className}`}
+      className={`text-btn  rounded-full bg-white px-6 py-3 text-black transition-all hover:scale-110 ${variants[variant]} ${className}}`}
       {...other}
     >
-      {leftIcon && (
-        <img className={'-ml-2 mr-2 h-8 w-8'} src={leftIcon} alt={''} />
-      )}
-      {children}
+      <div className={'flex items-center'}>
+        {leftIcon && (
+          <img className={'-ml-2 mr-2 h-8 w-8'} src={leftIcon} alt={''} />
+        )}
+        {children}
+      </div>
     </button>
   )
 }
