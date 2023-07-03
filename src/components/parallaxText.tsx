44abd88f5a1196c218432b341abab0ac6eee64c6
ctoverton <christianoverton@ctoverton.com>
2023-07-02 23:28:19 -0600

    better responsiveness on parallax text

--- src/components/parallaxText.tsx
+++ src/components/parallaxText.tsx
@@ -17,12 +17,12 @@ export const ParallaxText: React.FC<ParallaxTextProps> = ({
   const xFlipped = useTransform(scrollYProgress, [0, 1], ['0%', '-50%'])
 
   return (
-    <div className={className} {...other}>
+    <div className={`leading-none ${className}`} {...other}>
       <motion.section
         ref={scrollRef}
         style={{ x }}
         className={
-          'text-display-2 flex flex-nowrap space-x-8 whitespace-nowrap'
+          'text-display-2 mb-2 flex flex-nowrap space-x-8 whitespace-nowrap'
         }
       >
         <span className={'opacity-50'}>Javascript</span>
@@ -34,8 +34,19 @@ export const ParallaxText: React.FC<ParallaxTextProps> = ({
         <span>Tailwind</span>
         <span className={'opacity-50'}>GIT</span>
         <span>React Native</span>
+        <span className={'opacity-50'}>Angular</span>
+        <span className={''}>Redux</span>
         <span className={'opacity-50'}>Javascript</span>
         <span>Typescript</span>
+        <span className={'opacity-50'}>React</span>
+        <span>Next.js</span>
+        <span className={'opacity-50'}>Firebase</span>
+        <span className={'opacity-50'}>AWS</span>
+        <span>Tailwind</span>
+        <span className={'opacity-50'}>GIT</span>
+        <span>React Native</span>
+        <span className={'opacity-50'}>Angular</span>
+        <span className={''}>Redux</span>
       </motion.section>
       <motion.section
         ref={scrollRef}
@@ -54,8 +65,16 @@ export const ParallaxText: React.FC<ParallaxTextProps> = ({
         <span className={'opacity-50'}>Secure</span>
         <span>Scalable</span>
         <span className={'opacity-50'}>Maintainable</span>
+        <span className={'opacity-50'}>Performance</span>
         <span>Stability</span>
         <span className={'opacity-50'}>UI/UX</span>
+        <span>Modern</span>
+        <span className={'opacity-50'}>CI/CD</span>
+        <span className={'opacity-50'}>Test driven</span>
+        <span>Performance</span>
+        <span className={'opacity-50'}>Secure</span>
+        <span>Scalable</span>
+        <span className={'opacity-50'}>Maintainable</span>
       </motion.section>
     </div>
   )
